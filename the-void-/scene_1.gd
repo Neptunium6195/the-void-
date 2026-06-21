3988e712fd6d273b71fdbef189884ad8a97c5303
Neptunium6195 <seratesarah@gmail.com>
2026-06-21 01:06:41 -0700

    scene1b

--- the-void-/scene_1.gd
+++ the-void-/scene_1.gd
@@ -7,6 +7,7 @@ extends Node2D
 func _ready() -> void:
 	Dialogic.timeline_ended.connect(_on_dialogic_timeline_ended)
 	video.finished.connect(_on_video_stream_player_finished)
+	video2.finished.connect(_on_video_stream_player_2_finished)
 	
 	video.stream = load("res://Assets/Untitled_Artwork-4.ogv")
 	video2.stream = load("res://Assets/Untitled_Artwork-2.ogv")
@@ -23,6 +24,7 @@ func _process(delta: float) -> void:
 	pass
 
 func _on_video_stream_player_finished() -> void:
+	await get_tree().create_timer(1).timeout
 	Dialogic.start("res://scene1.dtl")
 	video.visible = false
 	#video2.visible = true
@@ -33,3 +35,10 @@ func _on_dialogic_timeline_ended() -> void:
 	print("Timeline finished! Starting video 2 now.")
 	video2.visible = true
 	video2.play()
+
+
+func _on_video_stream_player_2_finished() -> void:
+	await get_tree().create_timer(1).timeout
+	video2.visible = false
+	Dialogic.start("res://scene1b.dtl")
+	pass # Replace with function body.
